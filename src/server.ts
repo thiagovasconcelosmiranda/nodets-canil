e9681dc533519a35190e382fb657cea20b0505f7
Thiago Programador <thiagovasconcelos.miranda12@gmail.com>
2023-05-06 11:02:51 -0300

    Nova Atualização

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mustache from 'mustache-express';
+const mustache = require('mustache-express');
 import path from 'path';
 
 import mainRoutes from './routes';
